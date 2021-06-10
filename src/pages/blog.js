e2bef76efee98c801597f2bc42e56ef856a7aa82
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-06-10 13:13:24 -0400

    Add pages for about, portfolio, and blog.

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const BlogPage = () => {
+    return (
+        <main>
+            What up, world?
+        </main>
+    )
+}
+
+export default BlogPage
---
+++ src/pages/portfolio.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const PortfolioPage = () => {
+    return (
+        <main>
+            My works
+        </main>
+    )
+}
+
+export default PortfolioPage
