7fc02b35f99d71d0aa68071d47ef3bbd2e62bd82
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-06-18 06:23:22 -0400

    Installed react-bootstrap and react-reveal, added Hero component.

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from 'react-bootstrap'
+
+export default function Hero() {
+    return (
+        <section id="hero" className="jumbotron">
+            <Container>
+                
+            </Container>
+        </section>
+    )
+}
