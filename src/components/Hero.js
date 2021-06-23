c8982fb1c9d585afa55ad40a2e7a7c0d61557e38
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-06-22 23:09:14 -0400

    Installed 'gatsby-plugin-image' to handle images in gatsby.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,17 +2,24 @@ import React from 'react'
 
 import { Container, Row, Col } from 'react-bootstrap'
 
+import { StaticImage } from 'gatsby-plugin-image'
+
 export default function Hero() {
     return (
         <section id="hero" className="jumbotron">
             <Container>
                 <Row>
-                    <Col md={6} sm={12}></Col>
-                    <Col md={6} sm={12}></Col>
+                    <Col md={6} sm={12}>
+                        
+                        <StaticImage src="../images/Peter_Golden_Gate.JPG" alt="Profile Picture" />
+                        
+                    </Col>
+                    <Col md={6} sm={12}>
+                        <div>
+                            <title>Designer, Developer, Data Science</title>
+                        </div>
+                    </Col>
                 </Row>
-                <h1>
-                    Hello, world!
-                </h1>
             </Container>
         </section>
     )
