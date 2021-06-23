42b42cef21870007878de450adf031a1241e99fe
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-06-22 23:10:32 -0400

    Removed react-helmet meta data as it was breaking app.

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import * as React from 'react'
 import BlogPage from './BlogPage'
 import Home from './Home'
 import Hero from './Hero'
+import About from './About'
 import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 const App = () => {
     return (
-        <Hero />
+        <>
+            <Hero />
+            <About />
+        </>
     );
 }
 
