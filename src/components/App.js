d777abdd397f32ece8bf429a9243ef3a3c767e21
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-07-31 22:57:08 -0400

    Updated App.js to import and include Project.js and Contact.js components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import * as React from 'react'
 import BlogPage from './BlogPage'
 import Home from './Home'
+
 import Hero from './Hero'
+import NavBar from './NavBar'
 import About from './About'
+import Projects from './Projects'
+import Contact from './Contact'
+
 import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 const App = () => {
     return (
         <>
-            
             <Hero />
+            <NavBar />
             <About />
+            <Projects />
+            <Contact />
         </>
     );
 }
