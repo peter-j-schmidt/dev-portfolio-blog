bc1f071253d0226861b6ace0a7047ecf302446e1
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-07-31 22:55:41 -0400

    Added component for future projects page.

---
+++ src/components/Projects.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { Container, Row, Col } from 'react-bootstrap'
+
+import { StaticImage } from 'gatsby-plugin-image'
+
+import AboutImage from './AboutImage'
+
+export default function Projects() {
+    return (
+        
+         <div id="about">
+             <Container fluid >
+                 <Row className="justify-content-center align-items-center">
+                     <Col md={12} >
+                        
+                        <AboutImage />
+                        
+                    </Col>
+                </Row> 
+            </Container>
+        </div>
+            
+        
+    )
+}
