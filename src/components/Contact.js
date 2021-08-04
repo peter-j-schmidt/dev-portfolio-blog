a74d0c37f59f91d441f2a6f5bb4878ee3edfb633
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-08-04 16:20:06 -0400

    Added divs in order to create stacked inputs.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,7 +14,39 @@ export default function Contact() {
                  <Row className="justify-content-center align-items-center">
                      <Col md={12} >
                         
-                        <AboutImage />
+                        <form name="contact" method="POST" data-netlify="true" >
+
+                            <div>
+                                <label>
+                                    First Name
+                                    <input type="text" name="firstName"></input>
+                                </label>
+                            </div>
+
+                            <div>
+                                <label>
+                                    Last Name
+                                    <input type="text" name="lastName"></input>
+                                </label>
+                            </div>
+
+                            <div>
+                                <label>
+                                    Email
+                                    <input type="text" name="email"></input>
+                                </label>
+                            </div>
+
+                            <div>
+                                <label>
+                                    First Name
+                                    <textarea name="message"></textarea>
+                                </label>
+                            </div>
+
+                            <button type="submit" >Send</button>
+                            <input type="reset" value="Clear" />
+                        </form>
                         
                     </Col>
                 </Row> 
