8a080a12eed54407ba25f66884033248faaeed88
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-12-02 13:49:16 -0500

    Updated gatsby site configurations.

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,19 +16,5 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/posts/`,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-page-creator",
-      options: {
-        path: `${__dirname}/src/posts`,
-      },
-    },
-    `gatsby-plugin-mdx`,
   ],
 };
