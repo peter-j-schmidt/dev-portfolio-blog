8f2eba173910950dc6d3d038ae891f0b7562b037
peter-j-schmidt <schmidt.peter.john@gmail.com>
2022-01-04 22:17:29 -0500

    Added projects index page for future use with projects lists

---
+++ src/pages/projects.js
@@ -0,0 +1,23 @@
+import * as React from "react"
+
+import Layout from '../components/Layout'
+
+import { Container, Row, Col } from 'react-bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import '../style/main.scss'
+
+
+// markup
+const ProjectsIndex = () => {
+  return (
+    <>
+      <Layout>
+        
+      </Layout>
+    </>
+  )
+}
+
+export default ProjectsIndex
