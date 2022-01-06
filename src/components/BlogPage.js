d7cf874d0d6cecbb6633f3a4308cf0c1bfe1f828
peter-j-schmidt <schmidt.peter.john@gmail.com>
2022-01-06 17:29:04 -0500

    Updated functions to include image data.

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,16 +1,18 @@
 import * as React from 'react'
-import { graphql } from "gatsby"
 
 import { Container, Row, Col } from 'react-bootstrap'
 
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
 import Layout from './Layout'
 
-const BlogPage = ({ data, pageContext }) => {
+const BlogPage = ({ pageContext }) => {
 
     const { post } = pageContext
 
     const  Title  = post.frontmatter.title
     const  Description  = post.frontmatter.description
+    const Image = getImage(post.frontmatter.images)
     const  html  = post.html
 
     const styleObj = {
@@ -27,7 +29,11 @@ const BlogPage = ({ data, pageContext }) => {
 
             <Container fluid>
               <Row variant="secondary" className="justify-content-center align-items-center">
-                <Col>
+                <Col className="justify-content-center">
+                  <h1 className=" d-flex justify-content-center align-items-center py-3 my-3">{Title}</h1>
+
+                  <GatsbyImage image={Image} alt="Image, taken from above, of a person sitting in front of three computer monitors." />
+
                   <div className="blogpost" dangerouslySetInnerHTML={{ __html: html }} style={styleObj} />
                 </Col>
               </Row>
