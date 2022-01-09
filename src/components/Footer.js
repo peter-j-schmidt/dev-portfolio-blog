4d8ed9a78996c11100b59217ab42d847ff693c8c
peter-j-schmidt <schmidt.peter.john@gmail.com>
2022-01-08 19:51:48 -0500

    Added copyright symbol and padding for styling.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div class="me-auto d-flex justify-content-center bg-dark text-light">
-            Copyright (c) 2021 - Peter J Schmidt
+        <div class="me-auto d-flex justify-content-center bg-dark text-light p-4">
+            <p>Copyright &copy; 2021 - Peter J Schmidt</p>
         </div>
     )
 }
