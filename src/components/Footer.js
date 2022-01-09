afade1ab5a1ffac7bc0340831023dd9a175e3dc6
peter-j-schmidt <schmidt.peter.john@gmail.com>
2022-01-08 20:27:01 -0500

    Added styling for footer component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div class="me-auto d-flex justify-content-center bg-dark text-light p-4">
+        <div className="d-flex justify-content-center align-items-center bg-dark text-light">
             <p>Copyright &copy; 2021 - Peter J Schmidt</p>
         </div>
     )
